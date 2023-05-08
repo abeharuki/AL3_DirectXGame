ac0b581c73d814bb7bf285ff80853aaa0182189b
haruki <haruki@MSI>
2023-05-08 12:58:36 +0900

    完了

--- player.cpp
+++ player.cpp
@@ -222,12 +222,15 @@ void Player::Update() {
 	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
 	worldTransform_.TransferMatrix();
 
+	const float kMoveLimitX = 640.0f;
+	
 
+	worldTransform_.translation_.x = max(worldTransform_.translation_.x, -kMoveLimitX);
 
 	ImGui::Begin("Debug1");
 	ImGui::Text(
-	    "PlayerPos %d.%d,%d", worldTransform_.translation_.x, worldTransform_.translation_.y,
-	    worldTransform_.translation_.z);
+	    "PlayerPos %d.%d,%d", move.x, worldTransform_.translation_.x,
+	    worldTransform_.matWorld_.m[3][0]);
 	ImGui::End();
 	
 }
--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,7 +1,7 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-
+#include "AxisIndicator.h"
 
 GameScene::GameScene() {}
 
@@ -32,170 +32,12 @@ void GameScene::Initialize() {
 	//デバックカメラの生成
 	debugCamera_ = new DebugCamera(1280, 720);
 
-}
-// 正射影行列
-Matrix4x4 GameScene::MakeOrthographicMatrix(
-    float left, float top, float right, float bottom, float nearClip, float farClip) {
+	//軸方向表示
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクション
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&viewprojection_);
 
-	assert(left != right);
-	assert(top != bottom);
-	Matrix4x4 MakeOrthographicMatrix;
-	MakeOrthographicMatrix.m[0][0] = 2.0f / (right - left);
-	MakeOrthographicMatrix.m[0][1] = 0.0f;
-	MakeOrthographicMatrix.m[0][2] = 0.0f;
-	MakeOrthographicMatrix.m[0][3] = 0.0f;
 
-	MakeOrthographicMatrix.m[1][0] = 0.0f;
-	MakeOrthographicMatrix.m[1][1] = 2.0f / (top - bottom);
-	MakeOrthographicMatrix.m[1][2] = 0.0f;
-	MakeOrthographicMatrix.m[1][3] = 0.0f;
-
-	MakeOrthographicMatrix.m[2][0] = 0.0f;
-	MakeOrthographicMatrix.m[2][1] = 0.0f;
-	MakeOrthographicMatrix.m[2][2] = 1 / (farClip - nearClip);
-	MakeOrthographicMatrix.m[2][3] = 0.0f;
-
-	MakeOrthographicMatrix.m[3][0] = (left + right) / (left - right);
-	MakeOrthographicMatrix.m[3][1] = (top + bottom) / (bottom - top);
-	MakeOrthographicMatrix.m[3][2] = nearClip / (nearClip - farClip);
-	MakeOrthographicMatrix.m[3][3] = 1.0f;
-
-	return MakeOrthographicMatrix;
-}
-// 回転X
-Matrix4x4 GameScene::MakeRotateXMatrix(float theta = 0) {
-	Matrix4x4 MakeRotateMatrix;
-	MakeRotateMatrix.m[0][0] = 1;
-	MakeRotateMatrix.m[0][1] = 0;
-	MakeRotateMatrix.m[0][2] = 0;
-	MakeRotateMatrix.m[0][3] = 0;
-	MakeRotateMatrix.m[1][0] = 0;
-	MakeRotateMatrix.m[1][1] = std::cos(theta);
-	MakeRotateMatrix.m[1][2] = std::sin(theta);
-	MakeRotateMatrix.m[1][3] = 0;
-	MakeRotateMatrix.m[2][0] = 0;
-	MakeRotateMatrix.m[2][1] = -std::sin(theta);
-	MakeRotateMatrix.m[2][2] = std::cos(theta);
-	MakeRotateMatrix.m[2][3] = 0;
-	MakeRotateMatrix.m[3][0] = 0;
-	MakeRotateMatrix.m[3][1] = 0;
-	MakeRotateMatrix.m[3][2] = 0;
-	MakeRotateMatrix.m[3][3] = 1;
-	return MakeRotateMatrix;
-}
-// Y
-Matrix4x4 GameScene::MakeRotateYMatrix(float theta = 0) {
-	Matrix4x4 MakeRotateMatrix;
-	MakeRotateMatrix.m[0][0] = std::cos(theta);
-	MakeRotateMatrix.m[0][1] = 0;
-	MakeRotateMatrix.m[0][2] = -std::sin(theta);
-	MakeRotateMatrix.m[0][3] = 0;
-	MakeRotateMatrix.m[1][0] = 0;
-	MakeRotateMatrix.m[1][1] = 1;
-	MakeRotateMatrix.m[1][2] = 0;
-	MakeRotateMatrix.m[1][3] = 0;
-	MakeRotateMatrix.m[2][0] = std::sin(theta);
-	MakeRotateMatrix.m[2][1] = 0;
-	MakeRotateMatrix.m[2][2] = std::cos(theta);
-	MakeRotateMatrix.m[2][3] = 0;
-	MakeRotateMatrix.m[3][0] = 0;
-	MakeRotateMatrix.m[3][1] = 0;
-	MakeRotateMatrix.m[3][2] = 0;
-	MakeRotateMatrix.m[3][3] = 1;
-	return MakeRotateMatrix;
-}
-
-// Z
-Matrix4x4 GameScene::MakeRotateZMatrix(float theta = 0) {
-	Matrix4x4 MakeRotateMatrix;
-	MakeRotateMatrix.m[0][0] = std::cos(theta);
-	MakeRotateMatrix.m[0][1] = std::sin(theta);
-	MakeRotateMatrix.m[0][2] = 0;
-	MakeRotateMatrix.m[0][3] = 0;
-	MakeRotateMatrix.m[1][0] = -std::sin(theta);
-	MakeRotateMatrix.m[1][1] = std::cos(theta);
-	MakeRotateMatrix.m[1][2] = 0;
-	MakeRotateMatrix.m[1][3] = 0;
-	MakeRotateMatrix.m[2][0] = 0;
-	MakeRotateMatrix.m[2][1] = 0;
-	MakeRotateMatrix.m[2][2] = 1;
-	MakeRotateMatrix.m[2][3] = 0;
-	MakeRotateMatrix.m[3][0] = 0;
-	MakeRotateMatrix.m[3][1] = 0;
-	MakeRotateMatrix.m[3][2] = 0;
-	MakeRotateMatrix.m[3][3] = 1;
-	return MakeRotateMatrix;
-}
-
-// スカラー倍
-Matrix4x4 GameScene::Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
-	Matrix4x4 multiply;
-	multiply.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] +
-	                   m1.m[0][3] * m2.m[3][0];
-	multiply.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] +
-	                   m1.m[0][3] * m2.m[3][1];
-	multiply.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] +
-	                   m1.m[0][3] * m2.m[3][2];
-	multiply.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] +
-	                   m1.m[0][3] * m2.m[3][3];
-
-	multiply.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] +
-	                   m1.m[1][3] * m2.m[3][0];
-	multiply.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] +
-	                   m1.m[1][3] * m2.m[3][1];
-	multiply.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] +
-	                   m1.m[1][3] * m2.m[3][2];
-	multiply.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] +
-	                   m1.m[1][3] * m2.m[3][3];
-
-	multiply.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] +
-	                   m1.m[2][3] * m2.m[3][0];
-	multiply.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] +
-	                   m1.m[2][3] * m2.m[3][1];
-	multiply.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] +
-	                   m1.m[2][3] * m2.m[3][2];
-	multiply.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] +
-	                   m1.m[2][3] * m2.m[3][3];
-
-	multiply.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] +
-	                   m1.m[3][3] * m2.m[3][0];
-	multiply.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] +
-	                   m1.m[3][3] * m2.m[3][1];
-	multiply.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] +
-	                   m1.m[3][3] * m2.m[3][2];
-	multiply.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] +
-	                   m1.m[3][3] * m2.m[3][3];
-
-	return multiply;
-};
-
-// アフィン変換
-Matrix4x4 GameScene::MakeAffineMatrix(
-    const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
-	// 回転
-	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
-
-	Matrix4x4 MakeAffineMatrix;
-	MakeAffineMatrix.m[0][0] = scale.x * rotateXYZMatrix.m[0][0];
-	MakeAffineMatrix.m[0][1] = scale.x * rotateXYZMatrix.m[0][1];
-	MakeAffineMatrix.m[0][2] = scale.x * rotateXYZMatrix.m[0][2];
-	MakeAffineMatrix.m[0][3] = 0;
-	MakeAffineMatrix.m[1][0] = scale.y * rotateXYZMatrix.m[1][0];
-	MakeAffineMatrix.m[1][1] = scale.y * rotateXYZMatrix.m[1][1];
-	MakeAffineMatrix.m[1][2] = scale.y * rotateXYZMatrix.m[1][2];
-	MakeAffineMatrix.m[1][3] = 0;
-	MakeAffineMatrix.m[2][0] = scale.z * rotateXYZMatrix.m[2][0];
-	MakeAffineMatrix.m[2][1] = scale.z * rotateXYZMatrix.m[2][1];
-	MakeAffineMatrix.m[2][2] = scale.z * rotateXYZMatrix.m[2][2];
-	MakeAffineMatrix.m[2][3] = 0;
-	MakeAffineMatrix.m[3][0] = translate.x;
-	MakeAffineMatrix.m[3][1] = translate.y;
-	MakeAffineMatrix.m[3][2] = translate.z;
-	MakeAffineMatrix.m[3][3] = 1;
-	return MakeAffineMatrix;
 }
 
 void GameScene::Update() { 
@@ -203,27 +45,25 @@ void GameScene::Update() {
 	debugCamera_->Update();
 	
 	#ifdef _DEBUG
-	if (input_->TriggerKey(DIK_X)) {
-		if (isDebugCameraActve_) {
-			debugCamera_->Update();
-			viewprojection_.matView = debugCamera_->GetViewProjection().matView;
-			viewprojection_.matProjection = debugCamera_->GetViewProjection().matProjection;
+	if (input_->TriggerKey(DIK_SPACE)) {
 
-			viewprojection_.TransferMatrix();
-
-		} else {
-		
-			viewprojection_.UpdateMatrix();
-		}
-	
-	
+		 isDebugCameraActve_ = true;
 	}
 
+	if (isDebugCameraActve_) {
+		 debugCamera_->Update();
+		 viewprojection_.matView = debugCamera_->GetViewProjection().matView;
+		 viewprojection_.matProjection = debugCamera_->GetViewProjection().matProjection;
+
+		 viewprojection_.TransferMatrix();
+
+	} else {
+
+		 viewprojection_.UpdateMatrix();
+	}
+	
+
 #endif 
-
-
-
-
 
 }
 
@@ -254,7 +94,7 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 	player_->Draw(viewprojection_);
-
+	
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
