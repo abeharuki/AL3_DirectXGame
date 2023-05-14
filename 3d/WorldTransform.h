ab751ff67f8e1a04a0a7858989dbdaaf53c0aec6
haruki <haruki@MSI>
2023-05-14 19:43:47 +0900

    d

--- 3d/WorldTransform.h
+++ 3d/WorldTransform.h
@@ -45,4 +45,22 @@ struct WorldTransform {
 	/// 行列を転送する
 	/// </summary>
 	void TransferMatrix();
+
+
+	void UpdateMatrix();
+
+	// 回転X
+	Matrix4x4 MakeRotateXMatrix(float theta);
+	// Y
+	Matrix4x4 MakeRotateYMatrix(float theta);
+
+	// Z
+	Matrix4x4 MakeRotateZMatrix(float theta);
+
+	// スカラー倍
+	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+	// アフィン変換
+	Matrix4x4
+	    MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
 };
