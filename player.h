218eaa0c6449e1886a5cda7837f81c941ae4aae9
haruki <haruki@MSI>
2023-05-14 19:53:51 +0900

    関数の追加

--- player.h
+++ player.h
@@ -27,7 +27,25 @@ public:
 	/// </summary>
 	void Draw(ViewProjection viewprojection);
 
-	
+	Matrix4x4 QuaternionMatrix(Vector4 quaternion);
+
+	//  クォータニオン作成
+	// q = w + xi + yj + zk
+	// 　axis　回転させる軸
+	// radian 回転させる角度
+	// return  回転させる軸と角度を決めてクォータニオンにする
+	// オイラー角の実数が入ってるとこを1,実数の値はradianに入れる
+	// (例：オイラー角が{5,0,0}だったら　axis{1,0,0} radian 5)
+	// マウスで視点移動する場合はradianにマウスの移動量を入れる(多分!)
+	Vector4 MakeQuaternion(Vector3& axis, float& radian);
+
+	// クォータニオンの掛け算
+	//  left   計算の左の項
+	//  right  計算の右の項
+	//  return 計算したクォータニオン
+	// 掛け算したクォータニオンは、それ自体 1 つの回転
+	// つまり(q1*q2)はq1で回転した後にq2さらに回転した結果になる
+	Vector4 CalcQuaternion(Vector4& q1, Vector4& q2);
 
 	void Attack();
 
