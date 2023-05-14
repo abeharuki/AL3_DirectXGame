676f949035badecbd597853f6f71e28ac2f1782c
haruki <haruki@MSI>
2023-05-14 19:41:16 +0900

    b

---
+++ Utility.cpp
@@ -0,0 +1,194 @@
+#include "Utility.h"
+
+// 回転X
+Matrix4x4 Utility::MakeRotateXMatrix(float theta = 0) {
+	Matrix4x4 MakeRotateMatrix;
+	MakeRotateMatrix.m[0][0] = 1;
+	MakeRotateMatrix.m[0][1] = 0;
+	MakeRotateMatrix.m[0][2] = 0;
+	MakeRotateMatrix.m[0][3] = 0;
+	MakeRotateMatrix.m[1][0] = 0;
+	MakeRotateMatrix.m[1][1] = std::cos(theta);
+	MakeRotateMatrix.m[1][2] = std::sin(theta);
+	MakeRotateMatrix.m[1][3] = 0;
+	MakeRotateMatrix.m[2][0] = 0;
+	MakeRotateMatrix.m[2][1] = -std::sin(theta);
+	MakeRotateMatrix.m[2][2] = std::cos(theta);
+	MakeRotateMatrix.m[2][3] = 0;
+	MakeRotateMatrix.m[3][0] = 0;
+	MakeRotateMatrix.m[3][1] = 0;
+	MakeRotateMatrix.m[3][2] = 0;
+	MakeRotateMatrix.m[3][3] = 1;
+	return MakeRotateMatrix;
+}
+// Y
+Matrix4x4 Utility::MakeRotateYMatrix(float theta = 0) {
+	Matrix4x4 MakeRotateMatrix;
+	MakeRotateMatrix.m[0][0] = std::cos(theta);
+	MakeRotateMatrix.m[0][1] = 0;
+	MakeRotateMatrix.m[0][2] = -std::sin(theta);
+	MakeRotateMatrix.m[0][3] = 0;
+	MakeRotateMatrix.m[1][0] = 0;
+	MakeRotateMatrix.m[1][1] = 1;
+	MakeRotateMatrix.m[1][2] = 0;
+	MakeRotateMatrix.m[1][3] = 0;
+	MakeRotateMatrix.m[2][0] = std::sin(theta);
+	MakeRotateMatrix.m[2][1] = 0;
+	MakeRotateMatrix.m[2][2] = std::cos(theta);
+	MakeRotateMatrix.m[2][3] = 0;
+	MakeRotateMatrix.m[3][0] = 0;
+	MakeRotateMatrix.m[3][1] = 0;
+	MakeRotateMatrix.m[3][2] = 0;
+	MakeRotateMatrix.m[3][3] = 1;
+	return MakeRotateMatrix;
+}
+
+// Z
+Matrix4x4 Utility::MakeRotateZMatrix(float theta = 0) {
+	Matrix4x4 MakeRotateMatrix;
+	MakeRotateMatrix.m[0][0] = std::cos(theta);
+	MakeRotateMatrix.m[0][1] = std::sin(theta);
+	MakeRotateMatrix.m[0][2] = 0;
+	MakeRotateMatrix.m[0][3] = 0;
+	MakeRotateMatrix.m[1][0] = -std::sin(theta);
+	MakeRotateMatrix.m[1][1] = std::cos(theta);
+	MakeRotateMatrix.m[1][2] = 0;
+	MakeRotateMatrix.m[1][3] = 0;
+	MakeRotateMatrix.m[2][0] = 0;
+	MakeRotateMatrix.m[2][1] = 0;
+	MakeRotateMatrix.m[2][2] = 1;
+	MakeRotateMatrix.m[2][3] = 0;
+	MakeRotateMatrix.m[3][0] = 0;
+	MakeRotateMatrix.m[3][1] = 0;
+	MakeRotateMatrix.m[3][2] = 0;
+	MakeRotateMatrix.m[3][3] = 1;
+	return MakeRotateMatrix;
+}
+
+// スカラー倍
+Matrix4x4 Utility::Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 multiply;
+	multiply.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] +
+	                   m1.m[0][3] * m2.m[3][0];
+	multiply.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] +
+	                   m1.m[0][3] * m2.m[3][1];
+	multiply.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] +
+	                   m1.m[0][3] * m2.m[3][2];
+	multiply.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] +
+	                   m1.m[0][3] * m2.m[3][3];
+
+	multiply.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] +
+	                   m1.m[1][3] * m2.m[3][0];
+	multiply.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] +
+	                   m1.m[1][3] * m2.m[3][1];
+	multiply.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] +
+	                   m1.m[1][3] * m2.m[3][2];
+	multiply.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] +
+	                   m1.m[1][3] * m2.m[3][3];
+
+	multiply.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] +
+	                   m1.m[2][3] * m2.m[3][0];
+	multiply.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] +
+	                   m1.m[2][3] * m2.m[3][1];
+	multiply.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] +
+	                   m1.m[2][3] * m2.m[3][2];
+	multiply.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] +
+	                   m1.m[2][3] * m2.m[3][3];
+
+	multiply.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] +
+	                   m1.m[3][3] * m2.m[3][0];
+	multiply.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] +
+	                   m1.m[3][3] * m2.m[3][1];
+	multiply.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] +
+	                   m1.m[3][3] * m2.m[3][2];
+	multiply.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] +
+	                   m1.m[3][3] * m2.m[3][3];
+
+	return multiply;
+};
+
+// 平行移動
+Matrix4x4 Utility::MakeTranselateMatrix(const Vector3& translate) {
+	Matrix4x4 MakeTranslateMatrix;
+	MakeTranslateMatrix.m[0][0] = 1;
+	MakeTranslateMatrix.m[0][1] = 0;
+	MakeTranslateMatrix.m[0][2] = 0;
+	MakeTranslateMatrix.m[0][3] = 0;
+	MakeTranslateMatrix.m[1][0] = 0;
+	MakeTranslateMatrix.m[1][1] = 1;
+	MakeTranslateMatrix.m[1][2] = 0;
+	MakeTranslateMatrix.m[1][3] = 0;
+	MakeTranslateMatrix.m[2][0] = 0;
+	MakeTranslateMatrix.m[2][1] = 0;
+	MakeTranslateMatrix.m[2][2] = 1;
+	MakeTranslateMatrix.m[2][3] = 0;
+	MakeTranslateMatrix.m[3][0] = translate.x;
+	MakeTranslateMatrix.m[3][1] = translate.y;
+	MakeTranslateMatrix.m[3][2] = translate.z;
+	MakeTranslateMatrix.m[3][3] = 1;
+	return MakeTranslateMatrix;
+};
+
+// アフィン変換
+Matrix4x4 Utility::MakeAffineMatrix(
+    const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	// 回転
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
+	Matrix4x4 MakeAffineMatrix;
+	MakeAffineMatrix.m[0][0] = scale.x * rotateXYZMatrix.m[0][0];
+	MakeAffineMatrix.m[0][1] = scale.x * rotateXYZMatrix.m[0][1];
+	MakeAffineMatrix.m[0][2] = scale.x * rotateXYZMatrix.m[0][2];
+	MakeAffineMatrix.m[0][3] = 0;
+	MakeAffineMatrix.m[1][0] = scale.y * rotateXYZMatrix.m[1][0];
+	MakeAffineMatrix.m[1][1] = scale.y * rotateXYZMatrix.m[1][1];
+	MakeAffineMatrix.m[1][2] = scale.y * rotateXYZMatrix.m[1][2];
+	MakeAffineMatrix.m[1][3] = 0;
+	MakeAffineMatrix.m[2][0] = scale.z * rotateXYZMatrix.m[2][0];
+	MakeAffineMatrix.m[2][1] = scale.z * rotateXYZMatrix.m[2][1];
+	MakeAffineMatrix.m[2][2] = scale.z * rotateXYZMatrix.m[2][2];
+	MakeAffineMatrix.m[2][3] = 0;
+	MakeAffineMatrix.m[3][0] = translate.x;
+	MakeAffineMatrix.m[3][1] = translate.y;
+	MakeAffineMatrix.m[3][2] = translate.z;
+	MakeAffineMatrix.m[3][3] = 1;
+	return MakeAffineMatrix;
+}
+
+Vector3 Utility::Transform(const Vector3& vector, const Matrix4x4& matrix) {
+	Vector3 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] +
+	           1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] +
+	           1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] +
+	           1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] +
+	          1.0f * matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
+};
+
+// 加算
+Vector3 Utility::Add(const Vector3& v1, const Vector3& v2) {
+	Vector3 add;
+	add.x = v1.x + v2.x;
+	add.y = v1.y + v2.y;
+	add.z = v1.z + v2.z;
+	return add;
+};
+
+Vector3 Utility::TransformNormal(const Vector3& vector, const Matrix4x4& matrix) {
+	Vector3 result;
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2];
+
+	return result;
+};
