a136ffc383ee7226a3b36b7abeefad2c2897be1c
haruki <haruki@MSI>
2023-05-16 02:36:58 +0900

    背景の追加

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,6 +4,95 @@
 #include "AxisIndicator.h"
 
 
+void GameScene::CheckAllCollision() {
+	// 判定対象AとBの座標
+	Vector3 posA, posB;
+
+	// 自弾リスト取得
+	const std::list<PlayerBullet*>& playerBullets = player_->GetBullets();
+	// 敵弾リスト
+	const std::list<EnemyBullet*>& enemyBullets = enemy_->GetBullets();
+
+#pragma region 自キャラと敵弾の当たり判定
+	// 自キャラ座標
+	posA = player_->GetWorldPosition();
+
+	// 自キャラと敵弾全ての当たり判定
+	for (EnemyBullet* bullet : enemyBullets) {
+		// 敵弾の座標
+		posB = bullet->GetWorldPosition();
+
+		// 距離
+		float distance = (posB.x - posA.x) * (posB.x - posA.x) +
+		                 (posB.y - posA.y) * (posB.y - posA.y) +
+		                 (posB.z - posA.z) * (posB.z - posA.z);
+		float R1 = 1.0f;
+		float R2 = 1.0f;
+		if (distance <= (R1 + R2) * (R1 + R2)) {
+			// 自キャラの衝突時コールバックを呼び出す
+			player_->OnCollision();
+			// 敵弾の衝突時コールバックを呼び出す
+			bullet->OnCollision();
+		}
+	}
+
+#pragma endregion
+
+#pragma region 自弾と敵キャラの当たり判定
+
+	// 敵座標
+	posA = enemy_->GetWorldPosition();
+
+	// 自弾と敵キャラの当たり判定
+	for (PlayerBullet* bullet : playerBullets) {
+		// 自キャラの弾の座標
+		posB = bullet->GetWorldPosition();
+
+		// 距離
+		float distance = (posB.x - posA.x) * (posB.x - posA.x) +
+		                 (posB.y - posA.y) * (posB.y - posA.y) +
+		                 (posB.z - posA.z) * (posB.z - posA.z);
+		float R1 = 1.0f;
+		float R2 = 1.0f;
+		if (distance <= (R1 + R2) * (R1 + R2)) {
+			// 敵の衝突時コールバックを呼び出す
+			player_->OnCollision();
+			// 自キャラ弾の衝突時コールバックを呼び出す
+			bullet->OnCollision();
+		}
+	}
+
+#pragma endregion
+
+#pragma region 自弾と敵弾の当たり判定
+
+	// 自弾と敵キャラの当たり判定
+	for (PlayerBullet* bullet : playerBullets) {
+
+		for (EnemyBullet* enemybullet : enemyBullets) {
+
+			// 自キャラの弾の座標
+			posA = bullet->GetWorldPosition();
+			// 敵弾の座標
+			posB = enemybullet->GetWorldPosition();
+
+			// 距離
+			float distance = (posB.x - posA.x) * (posB.x - posA.x) +
+			                 (posB.y - posA.y) * (posB.y - posA.y) +
+			                 (posB.z - posA.z) * (posB.z - posA.z);
+			float R1 = 1.0f;
+			float R2 = 1.0f;
+			if (distance <= (R1 + R2) * (R1 + R2)) {
+				// 敵弾の衝突時コールバックを呼び出す
+				enemybullet->OnCollision();
+				// 自キャラ弾の衝突時コールバックを呼び出す
+				bullet->OnCollision();
+			}
+		}
+	}
+
+#pragma endregion
+}
 
 
 GameScene::GameScene() {}
@@ -14,100 +103,10 @@ GameScene::~GameScene() {
    delete player_;
    delete debugCamera_;
    delete enemy_;
+   delete modelSkydome_;
 }
 
 
-void GameScene::CheckAllCollision() {
-   // 判定対象AとBの座標
-   Vector3 posA, posB;
-
-   // 自弾リスト取得
-   const std::list<PlayerBullet*>& playerBullets = player_->GetBullets();
-   // 敵弾リスト
-   const std::list<EnemyBullet*>& enemyBullets = enemy_->GetBullets();
-
-#pragma region 自キャラと敵弾の当たり判定
-   // 自キャラ座標
-   posA = player_->GetWorldPosition();
-
-   // 自キャラと敵弾全ての当たり判定
-   for (EnemyBullet* bullet : enemyBullets) {
-	   // 敵弾の座標
-	   posB = bullet->GetWorldPosition();
-
-	   // 距離
-	   float distance = (posB.x - posA.x) * (posB.x - posA.x) +
-		                (posB.y - posA.y) * (posB.y - posA.y) +
-		                (posB.z - posA.z) * (posB.z - posA.z);
-	   float R1 = 1.0f;
-	   float R2 = 1.0f;
-	   if (distance <= (R1 + R2) * (R1 + R2)) {
-		   // 自キャラの衝突時コールバックを呼び出す
-		   player_->OnCollision();
-		   // 敵弾の衝突時コールバックを呼び出す
-		   bullet->OnCollision();
-		  
-	   }
-   }
-
-#pragma endregion
-
-#pragma region 自弾と敵キャラの当たり判定
-
-    // 敵座標
-   posA = enemy_->GetWorldPosition();
-
-   // 自弾と敵キャラの当たり判定
-   for (PlayerBullet* bullet : playerBullets) {
-	   // 自キャラの弾の座標
-	   posB = bullet->GetWorldPosition();
-
-	   // 距離
-	   float distance = (posB.x - posA.x) * (posB.x - posA.x) +
-		                (posB.y - posA.y) * (posB.y - posA.y) +
-		                (posB.z - posA.z) * (posB.z - posA.z);
-	   float R1 = 1.0f;
-	   float R2 = 1.0f;
-	   if (distance <= (R1 + R2) * (R1 + R2)) {
-		   // 敵の衝突時コールバックを呼び出す
-		   player_->OnCollision();
-		   // 自キャラ弾の衝突時コールバックを呼び出す
-		   bullet->OnCollision();
-	   }
-   }
-
-#pragma endregion
-
-#pragma region 自弾と敵弾の当たり判定
-
-   // 自弾と敵キャラの当たり判定
-   for (PlayerBullet* bullet : playerBullets) {
-	   
-	   for (EnemyBullet* enemybullet : enemyBullets) {
-	   
-		   // 自キャラの弾の座標
-		   posA = bullet->GetWorldPosition();
-	       // 敵弾の座標
-		   posB = enemybullet->GetWorldPosition();
-
-		   // 距離
-		   float distance = (posB.x - posA.x) * (posB.x - posA.x) +
-			                (posB.y - posA.y) * (posB.y - posA.y) +
-			                (posB.z - posA.z) * (posB.z - posA.z);
-		   float R1 = 1.0f;
-		   float R2 = 1.0f;
-		   if (distance <= (R1 + R2) * (R1 + R2)) {
-			   // 敵弾の衝突時コールバックを呼び出す
-			   enemybullet->OnCollision();
-			   // 自キャラ弾の衝突時コールバックを呼び出す
-			   bullet->OnCollision();
-		   }
-	   
-	   }
-   }
-
-#pragma endregion
-}
 
 void GameScene::Initialize() {
 
@@ -122,6 +121,7 @@ void GameScene::Initialize() {
 	model_ = Model::Create();
 
 	// ビュープロジェクションの初期化
+	viewprojection_.farZ = 1000.0f;
 	viewprojection_.Initialize();
 
 	player_ = new Player();
@@ -133,9 +133,15 @@ void GameScene::Initialize() {
 	enemy_->SetPlayer(player_);
 	enemy_->Initialize(model_, enemytextureHandle_);
 	
+	//天球
+	skydome_ = new Skydome();
+	//3Dモデルの生成
+	modelSkydome_ = Model::CreateFromOBJ("skydome", true);
+	skydome_->Initialize(modelSkydome_);
 
 	//デバックカメラの生成
 	debugCamera_ = new DebugCamera(1280, 720);
+
 
 	//軸方向表示
 	AxisIndicator::GetInstance()->SetVisible(true);
@@ -150,6 +156,7 @@ void GameScene::Update() {
 	player_->Update(); 
 	debugCamera_->Update();
 	enemy_->Update();
+	skydome_->Update();
 	CheckAllCollision();
 
 
@@ -160,6 +167,7 @@ void GameScene::Update() {
 	}
 
 	if (isDebugCameraActve_) {
+		 debugCamera_->SetFarZ(1000.0f);
 		 debugCamera_->Update();
 		 viewprojection_.matView = debugCamera_->GetViewProjection().matView;
 		 viewprojection_.matProjection = debugCamera_->GetViewProjection().matProjection;
@@ -205,7 +213,9 @@ void GameScene::Draw() {
 	player_->Draw(viewprojection_);
 	//敵の描画
 	enemy_->Draw(viewprojection_);
-	
+	//天球
+	skydome_->Draw(viewprojection_);
+
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
